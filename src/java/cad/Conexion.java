81529f65e092a2ea3fdbb377561706a71de36fd8
D3RL3 <tvboxm9sx@gmail.com>
2019-12-28 10:37:43 -0500

    Se crean los sp-procedimientos almacenados, javabeans, los setter y getter, y se crea la conexion a la base de datos

--- src/java/cad/Conexion.java
+++ src/java/cad/Conexion.java
@@ -7,6 +7,7 @@ package cad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,10 +19,12 @@ public class Conexion {
     public static Connection conetar() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_db", "root", "")
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_db", "root", "");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
+        return null;   
 }
 }
